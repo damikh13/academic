19d429eac022fef1cdffef69aa21f58b90302a73
Danil Mikhaylov <damikh13@gmail.com>
2022-10-20 21:34:20 +0300

    added random generating of da array

--- hw3/hole_in_an_array.cpp
+++ hw3/hole_in_an_array.cpp
@@ -4,12 +4,30 @@ int main()
 {
     int N; std::cin >> N;
     int arr[N];
-    for (int i = 0; i < N; ++i) std::cin >> arr[i];
+    arr[0] = 0;
+    
+    // generating array with 0 (a hole) at 0_idx
+    srand(time(0));
+    bool used[N + 1];
+    for (int i = 1; i < N; ++i) {
+        int rand_val = rand() % N + 1;
+        if (!used[rand_val]) {
+            arr[i] = rand_val;
+            used[rand_val] = true;
+        } else --i;
+    }
 
-    int S = (N + 1) * (2 + N) / 2;
-    for (int i = 0; i < N; ++i) {
+    // printing out the array
+    for (int i = 1; i < N; ++i) {
+        std::cout << arr[i] << ' ';
+    }
+    std::cout << std::endl;
+
+    // finding a hole
+    int S = N * (N + 1) / 2;
+    for (int i = 1; i < N; ++i) {
         S -= arr[i];
     }
 
-    std::cout << S << std::endl;
+    std::cout << "hole is: " << S << std::endl;
 }
